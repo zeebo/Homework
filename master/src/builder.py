381a1c338392e850115b34af5658f649c0cd7c01
jeff <leterip@me.com>
2012-02-12 16:13:46 -0500

    added builder to run all the commands

---
+++ master/src/builder.py
@@ -0,0 +1,10 @@
+import subprocess
+
+for points in [10, 100, 1000, 10000]:
+	for top in [1, 2, 5, 10]:
+		print "Points: %d\tTop: %d" % (points, top)
+		subprocess.call(["/bin/bash", "runner.sh"], env={
+			"N": str(points),
+			"TOP": str(top),
+			"PATH": "/Users/zeebo/Code/go/bin",
+		})
